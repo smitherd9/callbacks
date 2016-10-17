bcfc117af008d69f133457e243a3e293f24bdab0
smitherd9 <smitherd9@gmail.com>
2016-10-17 14:24:55 +0000

    refactored using event emitter class

--- callback.js
+++ callback.js
@@ -1,20 +1,41 @@
+var events = require('events');
+
+var count = new events.EventEmitter();
+
+
+
+
 function call(onStart, onProgress, onEnd) {
     onStart();
+    for (var i = 1; i <= 100; i++){
+            console.log(i);
+          	onProgress(i);
+        }
+    onEnd();
+    
+}
+
+function call2(count) {
+    count.emit('start');
+    for (var i = 1; i <= 100; i++){
+            console.log(i);
+          	count.emit('progress', i);
+        }
+    count.emit('end');
     
 }
 
 function onStart(){
-        for (var i = 1; i <= 100; i++){
-            console.log[i];
-        }
+        console.log('Getting started!');
+  		
 }
 
 function onProgress(i){
-    if ((i % 10) == 0)
+    if ((i % 10) === 0)
     console.log("We've gone 10 more");
-    else if (i == 100) {
+    else if (i === 100) 
         onEnd();
-    }
+    
 }
 
 function onEnd(){
@@ -22,4 +43,15 @@ function onEnd(){
 }
         
         
-call(onStart, onProgress, onEnd);
+// call(onStart, onProgress, onEnd);
+// call(function(){
+//     console.log('Hello everyone.');
+// }, onProgress, onEnd);
+
+
+
+count.on('start', onStart);
+count.on('progress', onProgress);
+count.on('end', onEnd);
+
+call2(count);
